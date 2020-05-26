51e86bd6ba88cf22058e82ca2cc094339b675e35
15659281017 <55989178+15659281017@users.noreply.github.com>
2020-05-26 22:04:57 +0800

    main.py

---
+++ 20200526/代码/main.py
@@ -0,0 +1,18 @@
+# encoding:utf-8
+'''
+调用各个模块实现对图片的测试和显示结果
+'''
+from plot import show
+import predict
+from prepare_image import prepare_image
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+def detect(img_path):
+    model = predict.model()
+    results = prepare_image(img_path=img_path, model=model)
+    show(img_path=img_path, results=results)
+
+if __name__ == '__main__':
+
+    detect("dataset/test/glass/glass400.jpg")
