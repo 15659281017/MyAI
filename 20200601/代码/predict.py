c34b9991d005045a7f607df4c6a976eae0fbc554
15659281017 <55989178+15659281017@users.noreply.github.com>
2020-06-01 22:37:55 +0800

    predict.py

---
+++ 20200601/代码/predict.py
@@ -0,0 +1,54 @@
+# encoding:utf-8
+'''
+加载垃圾分类模型
+'''
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+def model():
+    img_width, img_height = 512, 384
+    if K.image_data_format() == 'channels_first':
+        input_shape = (3, img_width, img_height)
+    else:
+        input_shape = (img_width, img_height, 3)
+
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape, padding="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(128, (3, 3), padding="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(6))  # 6分类
+    model.add(Activation('softmax'))  # 采用Softmax
+
+    model.compile(loss='categorical_crossentropy',  # 多分类
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    model.load_weights("weights-improvement-35-0.69.h5") #0530
+
+    return model
